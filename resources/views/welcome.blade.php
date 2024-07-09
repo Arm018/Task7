514dcb22e4354bd90ec66398a2a33bb0452d2d72
Arm018 <arm0182626@gmail.com>
2024-07-09 16:49:40 +0400

    changed style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.header')
+
 @section('content')
     <div class="container mt-5">
-    <div class="text-center">
-        <h1>Welcome to the Bookstore</h1>
-        <div class="list-group">
-            <a href="{{ route('user.books.index') }}" class="list-group-item list-group-item-action">View Books</a>
-            <a href="{{ route('user.authors.index') }}" class="list-group-item list-group-item-action">View Authors</a>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="text-center">
+                    <h1>Welcome to the Bookstore</h1>
+                    <div class="list-group mt-4">
+                        <a href="{{ route('user.books.index') }}" class="list-group-item list-group-item-action">View Books</a>
+                        <a href="{{ route('user.authors.index') }}" class="list-group-item list-group-item-action">View Authors</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-
 @endsection
-
